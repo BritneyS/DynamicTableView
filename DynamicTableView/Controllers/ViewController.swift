6ee8e13b67c232aadd2605aa1413415e2aa95025
Britney Smith <brit.smith7@gmail.com>
2019-03-12 14:55:13 -0400

    Add directory, start table view implementation

---
+++ DynamicTableView/Controllers/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  DynamicTableView
+//
+//  Created by Britney Smith on 3/12/19.
+//  Copyright © 2019 Britney Smith. All rights reserved.
+//
+
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+}
+
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+    
+}
+
